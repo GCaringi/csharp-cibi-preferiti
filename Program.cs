a6522d3f16af2ab83b27cf4ecd570fdd01cbb387
GCaringi <giannicaringi94@gmail.com>
2022-09-15 15:18:16 +0200

    Add array and print lenght

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+string[] classification = new[] {"polaretti", "pasta", "pizza", "limone", "gelato", "carne", "ostriche", "champagne", "verdura", "patate"};
+
+Console.WriteLine("Lunghezza della classifica " + classification.Length);
+
